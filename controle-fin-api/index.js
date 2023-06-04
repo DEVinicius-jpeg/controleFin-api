c19af5f67c43e8281d50521b8867c66e7b56cda9
Vinicius <viniciusespindola13@gmail.com>
2023-06-04 15:10:45 -0300

    Atulizado o cors

--- controle-fin-api/index.js
+++ controle-fin-api/index.js
@@ -5,13 +5,21 @@ const connectDB = require("./db/connect");
 require("dotenv").config();
 const notFound = require('./middleware/not-found');
 const errorHandlerMiddleware = require('./middleware/error-handler');
+const cors = require('cors')
 
 app.use(express.json());
 
-app.use('/api/v1/payments', payments);
+// const corsOptions = {
+//     origin: 'http://127.0.0.1:5173/', // Substitua pela origem correta do seu aplicativo React
+//     methods: 'GET, POST, PUT, DELETE',
+// };
 
+app.use(cors());
+
+app.use('/api/v1/payments', payments);
 app.use(notFound);
 app.use(errorHandlerMiddleware);
+
 
 const port = 3000;
 
